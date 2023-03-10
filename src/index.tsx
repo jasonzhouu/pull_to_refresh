3b341938327dfe736563e20de4cfa361d0ec3aab
Jason Zhou <jason.zhou@hextrust.com>
2023-03-10 11:07:11 +0800

    ReactPullToRefreshComponent

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = createRoot(rootElement!);
 
 root.render(
     <StrictMode>
-        {/*<PullToRefresh/>*/}
-        <ReactPullToRefreshComponent/>
+        <PullToRefresh/>
+        {/*<ReactPullToRefreshComponent/>*/}
     </StrictMode>
 );
